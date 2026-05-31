5825aa26417d7c3aea5018bb3e952631b6cf24da
Julian Peralta <julianperalta202@gmail.com>
2026-05-31 18:00:27 -0300

    fix: Modificadas las rutas en server.js para solucionar problemas

--- core/server.js
+++ core/server.js
@@ -16,8 +16,8 @@ class Server {
     this.app.use(express.urlencoded({ extended: true }))
   }
 
-  //rutas () {
-    //this.app.use('/alumnos', require('../routes/alumno.routes'))
+  rutas () {
+    this.app.use('/alumnos', require('../routes/alumno.routes'))
     //this.app.use('/materias', require('../routes/extras/materia.routes'))
     //this.app.use('/notas', require('../routes/extras/nota.routes'))
     //this.app.use('/profesores', require('../routes/extras/profesor.routes'))
